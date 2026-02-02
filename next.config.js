47ddb09f152af05fb9e3101903c225c3c35e4bdf
snnytw-blip <s.nnytw@gmail.com>
2026-02-03 02:08:33 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -340,7 +340,8 @@ const nextConfig = {
     // 这里的配置既可以服务端获取到，也可以在浏览器端获取到
     THEMES: themes
   },
-  output: 'export'
+  output: 'export', // すでにあるはず
+  trailingSlash: true
 }
 
 module.exports = process.env.ANALYZE
