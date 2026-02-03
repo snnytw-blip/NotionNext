bf59746bf63991b996ced9eaa2c5266b4b0c28e0
snnytw-blip <s.nnytw@gmail.com>
2026-02-03 22:52:05 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,12 +42,10 @@ export async function getStaticProps(req) {
 
   const POST_PREVIEW_LINES = siteConfig(
     'POST_PREVIEW_LINES',
-// ...以下、既存のコード
-  const POST_PREVIEW_LINES = siteConfig(
-    'POST_PREVIEW_LINES',
     12,
     props?.NOTION_CONFIG
   )
+
   props.posts = props.allPages?.filter(
     page => page.type === 'Post' && page.status === 'Published'
   )
@@ -72,32 +70,20 @@ export async function getStaticProps(req) {
       post.blockMap = await getPostBlocks(post.id, 'slug', POST_PREVIEW_LINES)
     }
   }
-  
-// --- 追加：日付エラーを防ぐためのガード ---
-  if (props.posts) {
-    props.posts = props.posts.filter(post => {
-      const dateValue = post?.publishDate || post?.date?.start_date || post?.lastEditedTime
-      if (!dateValue) return false // 日付がない記事は除外
-      const d = new Date(dateValue)
-      return !isNaN(d.getTime()) // 不正な日付の記事は除外
-    })
-  }
-  // ------------------------------------
-  
+
   // 生成robotTxt
   generateRobotsTxt(props)
   // 生成Feed订阅
   generateRss(props)
   // 生成
   generateSitemapXml(props)
+  
   // 检查数据是否需要从algolia删除
   checkDataFromAlgolia(props)
   if (siteConfig('UUID_REDIRECT', false, props?.NOTION_CONFIG)) {
     // 生成重定向 JSON
     generateRedirectJson(props)
   }
-
-  // 生成全文索引 - 仅在 yarn build 时执行 && process.env.npm_lifecycle_event === 'build'
 
   delete props.allPages
 
